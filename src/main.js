aa100a5e27628bbe52bb70e5756cb833c2d599d9
wanghuanrong <2623225424@qq.com>
2021-03-10 17:53:27 +0800

    2021-03-10-17-53

--- src/main.js
+++ src/main.js
@@ -19,18 +19,36 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
+
+// 导入NProgress, 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
 // 设置全局的axios
 import axios from 'axios'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+
 // 在token验证的时候需要把token传到后台，这里就使用到了拦截器，把token赋给 config.headers.Authorization 传到后台
+
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
 axios.interceptors.request.use(config => {
-  console.log(config);
+  // console.log(config);
+  NProgress.start()
 
   config.headers.Authorization = sessionStorage.getItem("token");
 
   return config;
+})
+
+
+// response 拦截器中,  隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
 })
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ const Params = () => import("@/components/goods/params")
 const List = () => import("@/components/goods/List")
 const Add = () => import("@/components/goods/add")
 const Order = () => import("@/components/order/Order")
+const Report = () => import("@/components/report/Report")
 
 
 Vue.use(VueRouter)
@@ -62,6 +63,9 @@ const routes = [
       }, {
         path: '/orders',
         component: Order
+      }, {
+        path: '/reports',
+        component: Report
       }
     ]
   }
